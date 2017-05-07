65bd630b93b546b1b53cb51d6774d4a4a48be6f0
Nyein Minn Oo <nyeinminoo.kn@gmail.com>
2017-05-07 23:38:58 +0800

    Update db_connect.rb

--- db_connect.rb
+++ db_connect.rb
@@ -4,7 +4,7 @@ require 'active_record'
 ActiveRecord::Base.establish_connection(
 :adapter => "sqlserver",
 :host => "192.168.0.8",
-# :host => "202.83.105.214",
+
 # :host => "192.168.0.91",
 :database => "BASES",
 :username => "1stbase",
